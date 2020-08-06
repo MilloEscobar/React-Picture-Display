d5f70426e3a06bd1be1474ff616d16c8db203a7a
Jaime Escobar <milloescobar@gmail.com>
2020-08-06 10:48:18 -0600

    Adding amplify auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import "./index.css";
 import App from "./App";
+
+Amplify.configure(config);
 
 ReactDOM.render(<App />, document.getElementById("root"));
